ffbd21d692a588e5f9073cdd272ed28f1b22d046
Zrj <13794009590@163.com>
2024-04-27 19:58:15 +0800

    Spinach bundle added

--- src/main/java/zrj/spinach/registry/ModItems.java
+++ src/main/java/zrj/spinach/registry/ModItems.java
@@ -1,24 +1,20 @@
 package zrj.spinach.registry;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItem;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import zrj.spinach.Spinach;
-import zrj.spinach.SpinachDataGenerator;
-
-import java.util.Arrays;
 
 public class ModItems implements ModInitializer{
     public static final Item SPINACH = new Item(new Item.Settings());
+    public static final Item SPINACH_BUNDLE = new Item(new Item.Settings());
 
 
     @Override
     public void onInitialize() {
         Registry.register(Registries.ITEM,new Identifier("spinach","spinach"), SPINACH);
+        Registry.register(Registries.ITEM,new Identifier("spinach_bundle","spinach bundle"), SPINACH_BUNDLE);
     }
+
 }
